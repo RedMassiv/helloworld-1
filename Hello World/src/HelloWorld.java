62082b26fdaf2c91ff300e528b419cd784811908
vince <vince@Router-1.fritz.box>
2016-09-22 09:18:05 +0200

    Primzahlsuche

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,11 +1,17 @@
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		int x=1;
-		while (true){
-			System.out.print(x + "Hello World! \n");//testtest
-			x=x+1;
+		int n = 1000000; //Grenze der Primzahlsuche
+		for (int i = 2; i <= n; i++) {
+			boolean isPrimzahl = true; //ist es eine Primzahl?
+			for (int j = 2; j < i/2 && isPrimzahl; j++) { //Primzahl teilen
+				if ((i % j) == 0) {
+					isPrimzahl = false;
+				}
+			}
+			if (isPrimzahl) {
+				System.out.println(i + " ist eine Primzahl!");
+			}
 		}
 	}
 
