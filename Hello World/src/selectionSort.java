ca83ff13323434e9a70d13feb8d2c985deb1e7da
ratzupaltuff <ratzupaltuff@ratzupaltuff.localdomain>
2017-02-09 09:16:14 +0100

    swap

---
+++ Hello World/src/selectionSort.java
@@ -0,0 +1,25 @@
+
+public class selectionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] list = {5, 8, 3, 6, 8, 9, 4, 2};
+		for (int c=0; c<list.length; c++){
+			System.out.print("["+list[c]+"]");
+		}
+		System.out.print("\n");
+		swap(list,1,2);
+		for (int c=0; c<list.length; c++){
+			System.out.print("["+list[c]+"]");
+		}
+	}
+	public static void swap (int[] array, int position1, int position2){
+		int zwischenspeicher=array[position1];
+		array[position1]=array[position2];
+		array[position2]=zwischenspeicher;
+	}
+	public static int findMinimum(int[] array, int startIndex, int endIndex) {
+		for 
+		return 
+	}
+}
