45ae07fbcfc5439ecf430aa097a3da6f235e2cb5
Wilke.Vincent <Wilke.Vincent@SK-001C255EF7CA.2631.skool.net>
2017-02-16 09:15:44 +0100

    selection Sort

--- Hello World/src/selectionSort.java
+++ Hello World/src/selectionSort.java
@@ -1,54 +1,76 @@
 
 public class selectionSort {
-
+static int berechnungen;
+static int speicherzugriffe;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] list = {5, 8, 3, 6, 8, 9, 4, 2}; //array definieren
-		for (int c=0; c<list.length; c++){    //array anzeigen
-			System.out.print("["+list[c]+"]");
-		}
-		System.out.print("\n");
+		int[] list = {5, 8, 3, 6, 8, 9, 4, 2,2342, 0, -1, 2}; //array definieren
 		
-		
-		swap(list,1,2);
-		for (int c=0; c<list.length; c++){
-			System.out.print("["+list[c]+"]");
-		}
-		System.out.print("\n");
-		
-		
-		System.out.print(findMinimum(list,1,list.length));
-		for (int c=0; c<list.length; c++){
-			System.out.print("["+list[c]+"]");
-		}
+		System.out.print("Das unsortierte Array: \n");output(list);    //array anzeigen
+		System.out.print("Stellen den Arrays: " + list.length + "\n");
+		selectionSort(list);
 	}
-	
-	
-	
+	public static void selectionSort(int[] array){
+		for (int i=0; i<array.length;i++){
+			output(array);
+			int minimum =findMinimum(array,i,array.length);
+			if(i!=minimum)
+				swap(array,i,minimum);
+		}
+		output(array);
+		swap(array,3,5);
+		output(array);
+		if(isSorted(array)){
+			System.out.print("Das Array ist sortiert!" + "\n");
+		} else {
+			System.out.print("Das Array ist noch nicht sortiert!" + "\n");
+		}
+		System.out.print("Speicherzugriffe: "+speicherzugriffe + "\n");
+		System.out.print("Berechnungen: " + berechnungen + "\n");
+	}
 	
 	
 	public static void swap (int[] array, int position1, int position2){
 		int zwischenspeicher=array[position1];
 		array[position1]=array[position2];
 		array[position2]=zwischenspeicher;
+		speicherzugriffe++;
 	}
 	
 	
-	
 	public static int findMinimum(int[] array, int startIndex, int endIndex) {
-		int zwischenspeicher = array[startIndex];
-		int stelle = startIndex;
-		int kleinsteStelle = startIndex;
-		while (array.length < endIndex){
-			if (zwischenspeicher<array[stelle]){
-				//nothing
+		int currentMinimum = array[startIndex];
+		int position = startIndex;
+		for (int i=startIndex;i<(endIndex);i++){
+			if(array[i]<currentMinimum){
+				position=i;
+				currentMinimum = array[i];
+				berechnungen++;
 			}
-			else {
-				array[stelle] = zwischenspeicher;
-				kleinsteStelle=stelle;
-			}
-			stelle++;
 		}
-		return kleinsteStelle;
+		return position;
+	}
+	
+	
+	public static void output(int[] array){
+		for (int c=0; c<array.length; c++){
+			System.out.print("["+array[c]+"]");
+		}
+		System.out.print("\n");
+	}
+	
+	
+	public static boolean isSorted(int[] array){
+		boolean sorted=true;
+		int i=0;
+		int endIndex=array.length;
+		//for (int i=0; sorted && i<endIndex; i++){
+		while(sorted && i<endIndex){
+			if(array[i]<array[i+1]){
+				sorted=false;
+			}
+			i++;
+		}
+		return sorted;
 	}
 }
