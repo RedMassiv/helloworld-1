6a7bef8ded42940eadc064379b1c252c66615a96
ratzupaltuff <ratzupaltuff@ratzupaltuff.localdomain>
2017-02-17 16:55:03 +0100

    random generator

--- Hello World/src/selectionSort.java
+++ Hello World/src/selectionSort.java
@@ -1,24 +1,31 @@
+import java.util.Random;
+//import java.io.*;
 
 public class selectionSort {
 static int berechnungen;
 static int speicherzugriffe;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] list = {5, 8, 3, 6, 8, 9, 4, 2,2342, 0, -1, 2}; //array definieren
-		
+		//class GetUserInput{}
+		int[] list = new int[50];  	 //array definieren
+		fill(list);
 		System.out.print("Das unsortierte Array: \n");output(list);    //array anzeigen
 		System.out.print("Stellen den Arrays: " + list.length + "\n");
-		selectionSort(list);
+		selectionSortMethode(list);
 	}
-	public static void selectionSort(int[] array){
+	
+	public static void selectionSortMethode(int[] array){  //SelectionSort Methode
+		if(isSorted(array)){
+			System.out.print("Das Array ist sortiert!" + "\n");
+		} else {
+			System.out.print("Das Array ist noch nicht sortiert!" + "\n");
+		}
+		
 		for (int i=0; i<array.length;i++){
 			output(array);
 			int minimum =findMinimum(array,i,array.length);
 			if(i!=minimum)
 				swap(array,i,minimum);
 		}
-		output(array);
-		swap(array,3,5);
 		output(array);
 		if(isSorted(array)){
 			System.out.print("Das Array ist sortiert!" + "\n");
@@ -30,7 +37,7 @@ static int speicherzugriffe;
 	}
 	
 	
-	public static void swap (int[] array, int position1, int position2){
+	public static void swap (int[] array, int position1, int position2){  //Swap methode
 		int zwischenspeicher=array[position1];
 		array[position1]=array[position2];
 		array[position2]=zwischenspeicher;
@@ -59,18 +66,44 @@ static int speicherzugriffe;
 		System.out.print("\n");
 	}
 	
+	public static void fill(int[] array){
+		for (int c=0; c<array.length; c++){
+			Random rand = new Random(); 
+			array[c] = rand.nextInt(101) - 50;
+		}
+		System.out.print("\n");
+	}
+	
 	
 	public static boolean isSorted(int[] array){
 		boolean sorted=true;
 		int i=0;
 		int endIndex=array.length;
 		//for (int i=0; sorted && i<endIndex; i++){
-		while(sorted && i<endIndex){
-			if(array[i]<array[i+1]){
+		while(sorted && i<endIndex-1){
+			if(array[i]>array[i+1]){
 				sorted=false;
 			}
 			i++;
 		}
 		return sorted;
 	}
+	
+	/*public static int GetUserInput(){ 
+		//the data that will be entered by the user String name; 
+		//an instance of the BufferedReader class 
+		//will be used to read the data 
+		BufferedReader reader; 
+		//specify the reader variable 
+		//to be a standard input buffer 
+		reader = new BufferedReader(new InputStreamReader(System.in)); 
+		//ask the user for their name 
+		System.out.print("What is your name? "); 
+		//read the data entered by the user using 
+		//the readLine() method of the BufferedReader cimport java.io.*;lass 
+		//and store the value in the name variable 
+		name = reader.readLine(); 
+		return name;
+		} 
+	} */
 }
