4aab11d9f079147226a81b7dbe6efbfbdc2b711b
ratzupaltuff <ratzupaltuff@ratzupaltuff.localdomain>
2017-02-18 17:03:18 +0100

    i

--- Hello World/src/selectionSort.java
+++ Hello World/src/selectionSort.java
@@ -2,39 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.Scanner;
 //import java.io.*;
 
 public class selectionSort {
 static int berechnungen;
 static int speicherzugriffe;
 	public static void main(String[] args) throws IOException {
-		//class GetUserInput{}
-		System.out.print("Stellen den Arrays: ");
-		BufferedReader r;
-		r=new BufferedReader(new InputStreamReader(System.in));
-		String aLine=r.readLine();
-		int n=Integer.parseInt(aLine);
-		
-		System.out.print("Anfang der Zufallszahlen: ");
-		BufferedReader c;
-		c=new BufferedReader(new InputStreamReader(System.in));
-		String bLine=c.readLine();
-		int l=Integer.parseInt(bLine);
-		
-		System.out.print("Ende der Zufallszahlen: ");
-		BufferedReader d;
-		d=new BufferedReader(new InputStreamReader(System.in));
-		String cLine=d.readLine();
-		int m=Integer.parseInt(cLine);
-		
-		int[] list = new int[n];  	 //array definieren
-		fill(list, l, m);
-		System.out.print("Das unsortierte Array: \n");output(list);    //array anzeigen
-		System.out.print("Stellen den Arrays: " + list.length + "\n");
-		selectionSortMethode(list);
+		selectionSortMethode(prepareArray()); //selection sort anwenden
 	}
 	
+	public static int[] prepareArray() throws IOException{
+		int n = getUserInput("Stellen den Arrays:");
+
+		while (n<0){
+			System.out.println("Die länge des Arrays muss über 0 sein!");
+			n=getUserInput("Stellen den Arrays:");
+		}
+		
+		int l=getUserInput("Anfang der Zufallszahlen:");
+		
+		int m=getUserInput("Ende der Zufallszahlen:");
+		
+		while (m<l){
+			System.out.println("Das Ende muss über dem Anfang liegen!");
+			m=getUserInput("Ende der Zufallszahlen:");
+		}
+	
+		int[] list = new int[n];  	 //array definieren
+		
+		fill(list, l, m);  //array mit zufälligen werten füllen
+		
+		System.out.print("Das unsortierte Array: \n");outputArray(list);    //array anzeigen
+		
+		return list;
+	}
+	
+	
 	public static void selectionSortMethode(int[] array){  //SelectionSort Methode
+		
 		if(isSorted(array)){
 			System.out.print("Das Array ist sortiert!" + "\n");
 		} else {
@@ -42,18 +48,29 @@ static int speicherzugriffe;
 		}
 		
 		for (int i=0; i<array.length;i++){
-			output(array);
 			int minimum =findMinimum(array,i,array.length);
-			if(i!=minimum)
+			
+			if(i!=minimum){              //nur tauschen bei zahlen die höher sind und nicht gleich
 				swap(array,i,minimum);
+			}
+			
+			
+			if (array.length<20){		//bei kleinen arrays die einzelnen schritte anzeigen
+				outputArray(array);
+			} else {
+				ladeBalken(i,array.length);		//bei großen einen ladebalken
+			}
 		}
-		output(array);
-		if(isSorted(array)){
+		
+		outputArray(array);  			//das sortierte array anzeigen
+		
+		if(isSorted(array)){			//checken ob das array auch tatsächlich soriert ist
 			System.out.print("Das Array ist sortiert!" + "\n");
 		} else {
 			System.out.print("Das Array ist noch nicht sortiert!" + "\n");
 		}
-		System.out.print("Speicherzugriffe: "+speicherzugriffe + "\n");
+		
+		System.out.print("Speicherzugriffe: "+speicherzugriffe + "\n");		//Statistiken zeigen
 		System.out.print("Berechnungen: " + berechnungen + "\n");
 	}
 	
@@ -80,7 +97,7 @@ static int speicherzugriffe;
 	}
 	
 	
-	public static void output(int[] array){  //output methode
+	public static void outputArray(int[] array){  //output methode
 		for (int c=0; c<array.length; c++){
 			System.out.print("["+array[c]+"]");
 		}
@@ -95,8 +112,7 @@ static int speicherzugriffe;
 		System.out.print("\n");
 	}
 	
-	
-	public static boolean isSorted(int[] array){
+	public static boolean isSorted(int[] array){  //checken ob das array tatsächlich sortiert ist
 		boolean sorted=true;
 		int i=0;
 		int endIndex=array.length;
@@ -108,5 +124,37 @@ static int speicherzugriffe;
 			i++;
 		}
 		return sorted;
+	}
+	
+	public static int getUserInput(String frage, Scanner Scanner) throws IOException{  //user input auslesen
+		/*System.out.print(frage + " ");
+		BufferedReader r;
+		r=new BufferedReader(new InputStreamReader(System.in));
+		String aLine=r.readLine();*/
+		Scanner scanner = (Scanner);
+		do{
+			scanner = new Scanner(System.in);
+		}
+		while (!scanner.hasNextInt());
+		return Integer.parseInt(scanner);
+	}
+	
+	public static void ladeBalken(int geschafft, int vonMaximum){
+		double prozent = geschafft*100/vonMaximum;
+		int balken = 50;
+		System.out.print("[");
+		for (int i=0; i<prozent ; i++) {
+		
+			System.out.print("=");
+			i=i+1;
+			balken=balken-1;
+		}
+
+		while (balken>0) {
+			System.out.print(" ");
+			balken=balken-1;
+		}
+		System.out.print("] \n");
+		
 	}
 }
